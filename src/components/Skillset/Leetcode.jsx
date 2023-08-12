7b12b5ce41aab8e52033e87b357f08d07dcaa34d
YaWarRehman <yawerrehman9@gmail.com>
2023-08-11 22:55:13 -0700

    no changes

---
+++ src/components/Skillset/Leetcode.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Row } from "react-bootstrap";
+
+const Leetcode = () => {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+      <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
+        <strong className="yellow">LeetCode</strong> Stat
+      </h1>
+      
+    </Row>
+  );
+}
+
+export default Leetcode;
